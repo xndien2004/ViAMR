ceb69f9f0778273baff0da5e0cdd9712a1373f28
xndien2004 <xuandienk4@gmail.com>
2025-08-14 14:34:09 +0700

    update

--- scripts/main.sh
+++ scripts/main.sh
@@ -11,5 +11,4 @@ python3 -m src.main \
     --input_file "/home/fit02/dien-workspace/viamr/src/data/public_test.txt" \
     --output_file "/home/fit02/dien-workspace/viamr/results.txt" \
     --model_name "/home/fit02/dien-workspace/viamr/outputs/Qwen-1.7B-SFT-2" \
-    --my_test 0 \
-    2>&1 | tee main.log
+    --my_test 0 
